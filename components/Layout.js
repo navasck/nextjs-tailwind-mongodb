cabae28990933defa647917a0452a1c7bb40b013
navasck <cknavas123@gmail.com>
2022-10-31 13:35:57 +0530

    home page products added using tailwind css

--- components/Layout.js
+++ components/Layout.js
@@ -15,13 +15,21 @@ function Layout({ title, children }) {
       </Head>
       <div className="flex flex-col min-h-screen justify-between">
         <header>
-          <nav>
-            <Link href="/">
-              <a className="text-lg font-bold">CKN MEDIA</a>
+          <nav className="flex h-12 justify-between shadow items-center px-4">
+            <Link href="/" className="text-lg font-bold">
+              CKN MEDIA
             </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
           </nav>
         </header>
-        <main>{children}</main>
+        <main className="container m-auto mt-4 px-4">{children}</main>
         <footer className="flex h-10 justify-center items-center shadow-inner">
           <p>Copyright © 2022</p>
         </footer>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
-import Image from 'next/image'
-import Layout from '../components/layout'
-import styles from '../styles/Home.module.css'
+import Image from "next/image";
+import Layout from "../components/Layout";
+import Productitem from "../components/Productitem";
+import styles from "../styles/Home.module.css";
+import data from "../utils/data";
 
 export default function Home() {
   return (
     <>
-    <Layout>
-      Home Page
-    </Layout>
-      
+      <Layout title="Home Page">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map((product) => (
+            <Productitem product={product} key={product.slug}></Productitem>
+          ))}
+        </div>
+      </Layout>
     </>
-  )
+  );
 }
